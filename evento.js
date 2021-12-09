95f622f0bf1a0799783eb2aa670cf8d6204652cf
daniel almeida <daniel.7t@hotmail.com>
2021-12-09 20:17:23 -0300

    evento savanis

---
+++ evento.js
@@ -0,0 +1,27 @@
+let dataevento = 02122021
+
+if(dataevento<07122021) {
+    
+    console.log("data valida")
+}else{
+    console.log("data invalida")
+}
+
+
+
+let cadastroidade = 21
+
+if(cadastroidade>18) {
+    
+    console.log("cadastro autorizado")
+}else{
+    console.log("evento para maiores de 18")
+}
+
+var listadeparticipantes = ("carlos","manuel","rebeca","joana")
+
+if(listadeparticipantes.length < 100 ){
+    console.log("cadastro do participante permitido")
+}else{
+    console.log("lista de participante excedido")
+}
